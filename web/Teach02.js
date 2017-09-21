a8c0bc79ed342e47cb95b30fc1832a8254b4229c
CarsonWoodford <carsonwoodford@gmail.com>
2017-09-20 20:41:56 -0600

    Added more colors / description

--- web/Teach02.js
+++ web/Teach02.js
@@ -9,6 +9,30 @@ document.getElementById('colorButton').onclick = function() {
 		case "red":
 			document.getElementById('div1').style.backgroundColor = "red";
 			break;
+		case "yellow":
+			document.getElementById('div1').style.backgroundColor = "yellow";
+			break;
+		case "blue":
+			document.getElementById('div1').style.backgroundColor = "blue";
+			break;
+		case "green":
+			document.getElementById('div1').style.backgroundColor = "green";
+			break;
+		case "purple":
+			document.getElementById('div1').style.backgroundColor = "purple";
+			break;
+		case "orange":
+			document.getElementById('div1').style.backgroundColor = "orange";
+			break;
+		case "white":
+			document.getElementById('div1').style.backgroundColor = "white";
+			break;
+		case "black":
+			document.getElementById('div1').style.backgroundColor = "black";
+			break;
+		case "grey":
+			document.getElementById('div1').style.backgroundColor = "grey";
+			break;
 		default:
 			alert("incompatible color!");
 			break;
