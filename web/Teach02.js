6ed2f51adc080f1ee0622ef1b95df381d1ec29f1
CarsonWoodford <carsonwoodford@gmail.com>
2017-09-20 20:37:44 -0600

    Textbox, new button, functions

--- web/Teach02.js
+++ web/Teach02.js
@@ -3,3 +3,14 @@
 document.getElementById('button').onclick = function() {
 	alert("Button was pressed!");
 }
+
+document.getElementById('colorButton').onclick = function() {
+	switch(document.getElementById('colorSelector').value) {
+		case "red":
+			document.getElementById('div1').style.backgroundColor = "red";
+			break;
+		default:
+			alert("incompatible color!");
+			break;
+	}
+}
