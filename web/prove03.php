712af1905b0ba4c27bc9bffd6313710be217e245
CarsonWoodford <carsonwoodford@gmail.com>
2017-09-27 15:04:47 -0600

    Helping a friend

--- web/prove03.php
+++ web/prove03.php
@@ -1,6 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title> Mr. Habablab's Wonderous Treasures!</title>
+  <link href="prove03.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+  <?php
 session_start();
 
  $_SESSION['plutQuan'] = $_POST['plut'];
 echo $_SESSION['plutQuan'];
 ?>
+</body>
+
+
+</html>
+
