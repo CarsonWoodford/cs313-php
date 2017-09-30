7f20cf0fe97ace94d6f9a6c78c36029415972b0b
CarsonWoodford <carsonwoodford@gmail.com>
2017-09-30 13:31:23 -0600

    Added cart page

---
+++ web/cart.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Your Items</title>
+</head>
+
+<body>
+<?php
+session_start();
+if(isset($_SESSION['cart'])){
+	$count = 0;
+	echo "In your cart, you have:";
+	foreach($_SESSION['cart'] as $value){
+		echo $_SESSION['number'][count] . " " . $value;
+	}
+}
+else{
+	echo "cart is empty. Please add some items and try again!";
+}
+?>
+</body>
+</html>
