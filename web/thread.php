b26ccd4748e2624bbc69c81ad5118375706d57e3
CarsonWoodford <carsonwoodford@gmail.com>
2017-10-14 15:37:26 -0600

    Finishing touches

--- web/thread.php
+++ web/thread.php
@@ -9,7 +9,7 @@
 <body>
 
 <div class="container">
-  <div class="header"><a href="#"><img src="" alt="Insert Logo Here" name="Insert_logo" width="180" height="90" id="Insert_logo" style="background-color: #C6D580; display:block;" /></a> 
+  <div class="header"><a href="#"><img src="" alt="LogoPlaceholder" name="Insert_logo" width="180" height="90" id="Insert_logo" style="background-color: #C6D580; display:block;" /></a> 
     <!-- end .header --></div>
   <div class="sidebar1">
     <ul class="nav">
