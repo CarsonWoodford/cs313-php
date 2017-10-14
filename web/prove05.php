3e1ab9f5af1c2b9c282e9512126955913af9ab6f
CarsonWoodford <carsonwoodford@gmail.com>
2017-10-14 13:16:14 -0600

    Connected, trying to get data onto screen to format

--- web/prove05.php
+++ web/prove05.php
@@ -42,6 +42,7 @@
 		try
 		{
 			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 		catch (PDOException $ex)
 		{
