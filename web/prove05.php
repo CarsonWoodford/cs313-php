fe05b058251ecafcb2478ffc848fa33f2f3f308d
CarsonWoodford <carsonwoodford@gmail.com>
2017-10-14 08:42:09 -0600

    changed html to php

--- web/prove05.php
+++ web/prove05.php
@@ -2,11 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<!-- TemplateBeginEditable name="doctitle" -->
 <title>Untitled Document</title>
-<!-- TemplateEndEditable -->
-<!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
 <link href="prove05.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -24,9 +20,11 @@
     </ul>
     <p> The above links demonstrate a basic navigational structure using an unordered list styled with CSS. Use this as a starting point and modify the properties to produce your own unique look. If you require flyout menus, create your own using a Spry menu, a menu widget from Adobe's Exchange or a variety of other javascript or CSS solutions.</p>
     <p>If you would like the navigation along the top, simply move the ul.nav to the top of the page and recreate the styling.</p>
-    <!-- end .sidebar1 --></div>
+    </div><!-- end .sidebar1 -->
   <div class="content">
-    <h1>Instructions</h1>
+  	<?php
+    ?>
+    <h1>Changed</h1>
     <p>Be aware that the CSS for these layouts is heavily commented. If you do most of your work in Design view, have a peek at the code to get tips on working with the CSS for the fixed layouts. You can remove these comments before you launch your site. To learn more about the techniques used in these CSS Layouts, read this article at Adobe's Developer Center - <a href="http://www.adobe.com/go/adc_css_layouts">http://www.adobe.com/go/adc_css_layouts</a>.</p>
     <h2>Clearing Method</h2>
     <p>Because all the columns are floated, this layout uses a clear:both declaration in the .footer rule.  This clearing technique forces the .container to understand where the columns end in order to show any borders or background colors you place on the .container. If your design requires you to remove the .footer from the .container, you'll need to use a different clearing method. The most reliable will be to add a &lt;br class=&quot;clearfloat&quot; /&gt; or &lt;div  class=&quot;clearfloat&quot;&gt;&lt;/div&gt; after your final floated column (but before the .container closes). This will have the same clearing effect.</p>
