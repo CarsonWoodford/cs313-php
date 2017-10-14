2149ef454a65e74c25cca3447838c4849bb44519
CarsonWoodford <carsonwoodford@gmail.com>
2017-10-14 11:58:11 -0600

    still trying to connect

--- web/prove05.php
+++ web/prove05.php
@@ -38,9 +38,19 @@
     		die();
 		}
 		echo 'testing';*/
+		$dbUrl = getenv('DATABASE_URL');
+
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
 		try
 		{
-    		$db = new PDO("pgsql:host=ec2-23-23-248-162.compute-1.amazonaws.com;");
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		}
 		catch (PDOException $ex)
 		{
